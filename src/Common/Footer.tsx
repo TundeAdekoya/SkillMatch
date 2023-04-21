afc4a3ea29c94987c172277806360a5052b5806c
TundeAdekoya <adekoyagbolahan101@gmail.com>
2023-04-21 23:07:31 +0100

    footer page initiated

--- src/Common/Footer.tsx
+++ src/Common/Footer.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import '../css/Footer.css'
+import whiteIcon from '../Assets/logoIcon.svg'
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <div id='footer'>
+      <div id="topFooter">
+        <div className="topFooterSubSection">
+          <h3>Find techies easily</h3>
+          <div className="subscribeInput">
+            <form action="">
+              <input type="email" name="" id="" placeholder='Enter email'/>
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
+        </div>
+        <div id="toFooterLinkSecrion">
+              <ul>
+                <li>SkillMatch</li>
+                <li>Developer</li>
+                <li>Product</li>
+                <li>About</li>
+                <li>FAQ</li>
+              </ul>
+              
+              <ul>
+                <li>Developers</li>
+                <li>How - to - SkillMatch</li>
+                <li>Partners</li>
+              </ul>
+
+              <ul>
+                <li>Contact</li>
+                <li>Email</li>
+              </ul>
+        </div>
+      </div>
+      <div id="bottomFooter">
+        <div className='text'>
+          <p>@SkillMatch</p>
+          <p>{year}</p>
+        </div>
+          <img src={whiteIcon} alt="" />
+      </div>
+    </div>
   )
 }
 
