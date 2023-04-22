ec959b707610eb3db6cead2c4f1e155312e5ea53
TundeAdekoya <adekoyagbolahan101@gmail.com>
2023-04-22 14:17:32 +0100

    general fixes and vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import  {Route, Routes} from  'react-router-dom'
 import NavBar from './Common/NavBar'
 import Footer from './Common/Footer'
 // import './App.css';
